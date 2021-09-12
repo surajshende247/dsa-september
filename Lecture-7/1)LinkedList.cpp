6cbf4fb6099b6ac6d384e75a5be2cc3ff0f5ba6b
surajshende247 <surajshende247@gmail.com>
2021-09-12 17:58:36 +0530

    Lecture-7 Added

---
+++ Lecture-7/1)LinkedList.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+int main()
+{
+    Node *head = new Node();
+    Node *a = new Node();
+    Node *b = new Node();
+
+    head->data = 20;
+    a->data = 15;
+    b->data = 10;
+
+    head->next = a;
+    a->next = b;
+    b->next = NULL;
+
+    //traversing
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << ", ";
+        temp = temp->next;
+    }
+    return 0;
+}
